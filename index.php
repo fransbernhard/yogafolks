b0fe43544d1c1757fed584593e051ad730d45c20
fransbernhard <mimi.lundberg@frojd.se>
2020-10-11 21:50:54 +0200

    Cleanup

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-<div class="Container">
+<main class="main">
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
             <h1><?php the_title(); ?></h1>
             <p><?php the_content(); ?></p>
         <?php endwhile; ?>
     <?php endif; ?>
-</div>
+</main>
 
 <?php get_footer(); ?>
