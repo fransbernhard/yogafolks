3e2ce682649e16e200a01e1b35a1caa20f9ed868
fransbernhard <mimi.lundberg@frojd.se>
2020-12-13 11:37:31 +0100

    Remove scroll

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,6 @@
 'use strict';
 var $ =  jQuery;
 
-var $root = $('html, body');
-
 // RESPONSIVE MENU
 $(".js-toggle-menu").click(function() {
     $(".header").toggleClass("header--active");
@@ -15,23 +13,3 @@ $(".menu__list").find("li").find("a").click(function(){
 $(".social__item").click(function(){
     $(".header").removeClass("header--active");
 })
-
-// //Smooth scrolling with links
-// $('a[href*=\\#]').on('click', function(e){     
-//     e.preventDefault();
-    
-//     $root.animate({
-//         scrollTop: $(this.hash).offset().top
-//     }, 500, function() {
-//         window.location.hash = target;
-//     })
-// });
-
-// // Smooth scrolling when the document is loaded and ready
-// $(document).ready(function(){    
-//     e.preventDefault();
-//     $root.animate({
-//       scrollTop:$(location.hash).offset().‌​top
-//     }, 500);
-// });
-
